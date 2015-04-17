1a3ab0068926daeeea37e1eab1bf0e513d24baa7
oropon <momochitose@gmail.com>
2015-04-17 18:30:03 +0900

    add ASCIIコードを持たないキー入力を受け取る設定

--- conf/meltingpot.el
+++ conf/meltingpot.el
@@ -119,3 +119,22 @@
 
 ;; find-functionをキー割り当てする
 (find-function-setup-keys)
+
+;; Terminal上で、対応するアスキーコードのないキー入力が出来ない問題への対処
+;; cf.) http://d.akinori.org/2012/01/02/%E3%82%BF%E3%83%BC%E3%83%9F%E3%83%8A%E3%83%AB%E3%81%AEemacs%E3%81%A7%E3%82%82%E7%89%B9%E6%AE%8A%E3%82%AD%E3%83%BC%E3%82%B3%E3%83%B3%E3%83%9C/
+
+(defun event-apply-control-shift-modifier (ignore-prompt)
+  "\\Add the Control+Shift modifier to the following event.
+For example, type \\[event-apply-control-shift-modifier] SPC to enter Control-Shift-SPC."
+    (vector (event-apply-modifier
+             (event-apply-modifier (read-event) 'shift 25 "S-")
+             'control 26 "C-")))
+(define-key function-key-map (kbd "C-x @ C") 'event-apply-control-shift-modifier)
+
+(defun event-apply-meta-control-modifier (ignore-prompt)
+  "\\Add the Meta-Control modifier to the following event.
+For example, type \\[event-apply-meta-control-modifier] % to enter Meta-Control-%."
+    (vector (event-apply-modifier
+             (event-apply-modifier (read-event) 'control 26 "C-")
+             'meta 27 "M-")))
+(define-key function-key-map (kbd "C-x @ M") 'event-apply-meta-control-modifier)
